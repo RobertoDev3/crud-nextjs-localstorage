19c6ed877a24980bf66d0965cdcf71a8ced34a5c
RobertoDev3 <roberto.dev3@hotmail.com>
2025-04-24 18:52:11 -0300

    feat: replace placeholder text with task creation button in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
   return (
     <main className='p-4'>
-      <div>Hello world!</div>
+      <section className='bg-accent flex justify-end rounded-md p-4'>
+        <Button>Criar Tarefa</Button>
+      </section>
+      <section></section>
     </main>
   );
 }
