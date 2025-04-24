c8392b1384b88bd83adab29b7e4a8152a2837797
RobertoDev3 <roberto.dev3@hotmail.com>
2025-04-24 19:04:59 -0300

    feat: improve layout structure in Home and Nav components for better responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ import {
 
 export default function Home() {
   return (
-    <main className='flex flex-col gap-4 p-4'>
+    <main className='mx-auto flex max-w-7xl flex-col gap-4 p-4'>
       <section className='bg-accent flex justify-end rounded-md p-4'>
         <Button>Criar Tarefa</Button>
       </section>
--- src/components/layout/nav.tsx
+++ src/components/layout/nav.tsx
@@ -2,9 +2,11 @@ import { ThemeButton } from '../common/theme-button';
 
 export function Nav() {
   return (
-    <nav className='bg-secondary text-foreground flex items-center justify-between p-4'>
-      <h2 className='text-2xl font-semibold'>Crud Next.js Localstorage</h2>
-      <ThemeButton />
+    <nav className='bg-secondary'>
+      <div className='text-foreground mx-auto flex max-w-7xl items-center justify-between p-4'>
+        <h2 className='text-2xl font-semibold'>Crud Next.js Localstorage</h2>
+        <ThemeButton />
+      </div>
     </nav>
   );
 }
