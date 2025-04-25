c33990742989ce1eb5c503bd4199e30ee7af3150
RobertoDev3 <roberto.dev3@hotmail.com>
2025-04-25 12:30:01 -0300

    feat: replace header title with Link component for improved navigation

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,10 +1,13 @@
+import Link from 'next/link';
 import { ThemeButton } from '../common/theme-button';
 
 export function Header() {
   return (
     <header className='bg-secondary'>
       <div className='text-foreground mx-auto flex max-w-7xl items-center justify-between p-4'>
-        <h2 className='text-2xl font-semibold'>Crud Next.js Localstorage</h2>
+        <Link href='/' className='text-2xl font-semibold'>
+          Crud Next.js Localstorage
+        </Link>
         <ThemeButton />
       </div>
     </header>
