37a1faf09e388ac5c5371dca9586d4efc4140fd0
RobertoDev3 <roberto.dev3@hotmail.com>
2025-04-25 16:05:06 -0300

    feat: update layout of task display section to use grid for better responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,7 +43,7 @@ export default function Home() {
           }}
         />
       </section>
-      <section className='flex flex-wrap justify-center gap-4'>
+      <section className='grid grid-cols-1 gap-4 self-center md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
         {tasks.map(task => (
           <TaskCard
             key={task.id}
